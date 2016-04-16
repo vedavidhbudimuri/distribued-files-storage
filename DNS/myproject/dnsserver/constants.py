908c97fa540c9e697982335d31d967dfd325f8fa
vedavidhbudimuri <vedavidhbudimuri@gmail.com>
2016-04-16 21:42:57 +0530

    dns ip settings changed

--- DNS/myproject/dnsserver/constants.py
+++ DNS/myproject/dnsserver/constants.py
@@ -1,11 +1,11 @@
 
 
-BASE_IP = 'http://10.145.47.235'
+BASE_IP = 'http://10.42.0.2'
 
 
 FORWARD_TO_SERVERS = [
-                  BASE_IP+':8001/mainserver/',
-                  BASE_IP+':8002/mainserver/',
+                  'http://10.42.0.1:8001/mainserver/',
+                  'http://10.42.0.3:8002/mainserver/',
                   ]
 
 IP = BASE_IP+':7000/'
