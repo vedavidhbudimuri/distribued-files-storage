35254bedc7a737ca00321130b7e29c98908c11a1
vedavidhbudimuri <vedavidhbudimuri@gmail.com>
2016-04-16 21:10:22 +0530

    mainserver files

--- DNS/myproject/dnsserver/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-# from mainserver.models import Document 
-# admin.site.register(Document)
-#
-# from mainserver.models import FileSystem
-# admin.site.register(FileSystem)
-# Register your models here.
